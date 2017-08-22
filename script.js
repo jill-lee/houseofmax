6f5185fd63f03e95aec242fe4d8bb32b5d5f7cd5
Jill Lee <jilllee.ace@outlook.com>
2017-08-22 04:01:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,48 @@
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $(".opaque-navbar").addClass("opaque");
+  } else {
+    $(".opaque-navbar").removeClass("opaque");
+  }
+});
 
-  <script src="jquery.fixedbg.min.js"></script>
 
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js">
-</script>
+function Parallax() {
+  scrollPos = $(this).scrollTop();
+  $(".intro, .header, .bgimg-1").css({
+    opacity: 1 - scrollPos / 200
+  });
+  if (scrollPos > 200) {
+    $(".intro").css({});
+  }
+}
+$(document).ready(function() {
+  $(window).scroll(function() {
+    Parallax();
+  });
+});
 
-
-var navbarHeight = $('.navbar').height(); 
+// hover links to black //
 
 $(window).scroll(function() {
-  var navbarColor = "0,0,0";
-  var smallLogoHeight = $('.small-logo').height();
-  var bigLogoHeight = $('.big-logo').height();
-  
-  
-  var smallLogoEndPos = 0;
-  var smallSpeed = (smallLogoHeight / bigLogoHeight);
-  
-  var ySmall = ($(window).scrollTop() * smallSpeed); 
-  
-  var smallPadding = navbarHeight - ySmall;
-  if (smallPadding > navbarHeight) { smallPadding = navbarHeight; }
-  if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
-  if (smallPadding < 0) { smallPadding = 0; }
-  
-  $('.small-logo-container ').css({ "padding-top": smallPadding});
-  
-  var navOpacity = ySmall / smallLogoHeight; 
-  if  (navOpacity > 1) { navOpacity = 1; }
-  if (navOpacity < 0 ) { navOpacity = 0; }
-  var navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
-  $('.navbar').css({"background-color": navBackColor});
-  
-  var shadowOpacity = navOpacity * 0.4;
-  if ( ySmall > 1) {
-    $('.navbar').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
+  var height = $(window).scrollTop();
+  if (height > 200) {
+    $(".switch-text").hover(
+      function() {
+        $(this).css("color", "#000");
+      },
+      function() {
+        $(this).css("color", "#9d9d9d");
+      }
+    );
   } else {
-    $('.navbar').css({"box-shadow": "none"});
+    $(".switch-text").hover(
+      function() {
+        $(this).css("color", "#FFF");
+      },
+      function() {
+        $(this).css("color", "#9d9d9d");
+      }
+    );
   }
-  
 });
-
-
-(function( $ ) {
-
-    //Function to animate slider captions 
-	function doAnimations( elems ) {
-		//Cache the animationend event in a variable
-		var animEndEv = 'webkitAnimationEnd animationend';
-		
-		elems.each(function () {
-			var $this = $(this),
-				$animationType = $this.data('animation');
-			$this.addClass($animationType).one(animEndEv, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-	
-	//Variables on page load 
-	var $myCarousel = $('#sg-carousel'),
-		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-		
-	//Initialize carousel 
-	$myCarousel.carousel();
-	
-	//Animate captions in first slide on page load 
-	doAnimations($firstAnimatingElems);
-	
-	//Pause carousel  
-	$myCarousel.carousel('pause');
-	
-	
-	//Other slides to be animated on carousel slide event 
-	$myCarousel.on('slide.bs.carousel', function (e) {
-		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-		doAnimations($animatingElems);
-	});  
-   
-	
-})(jQuery);	
-
-
-//6 ITEMS SLIDER
-
-$(document).ready(function(){
-
-$('#itemslider').carousel({ interval: 3000 });
-
-$('.carousel-showmanymoveone .item').each(function(){
-var itemToClone = $(this);
-
-for (var i=1;i<6;i++) {
-itemToClone = itemToClone.next();
-
-if (!itemToClone.length) {
-itemToClone = $(this).siblings(':first');
-}
-
-itemToClone.children(':first-child').clone()
-.addClass("cloneditem-"+(i))
-.appendTo($(this));
-}
-});
-});
-
-
-
-
-  
